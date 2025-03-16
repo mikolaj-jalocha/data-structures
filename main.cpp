8ffa5c2306d4cdef4ea838015c68af169a0469f0
mikolaj-jalocha <mikics2k17@gmail.com>
2025-03-16 13:34:37 +0100

    feat: add tests to main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include "include/singly_linked_list.h"
 #include <iostream>
 
+#include "include/doubly_linked_list.h"
+
 using namespace std;
 int main() {
-     SinglyLinkedList<int> list;
+     DoublyLinkedList<int> list;
 
      list.push_first(10);
      list.push_first(20);
      list.push_back(30);
-     list.push(1, 25); // Insert 25 at index 1
+     list.push( 1, 25);
 
      std::cout << "List after insertions: ";
      list.display();
@@ -16,7 +18,7 @@ int main() {
      std::cout << "First element: " << list.first() << "\n";
      std::cout << "Last element: " << list.last() << "\n";
 
-     list.remove(1); // Remove element at index 1
+     list.remove(1);
      std::cout << "List after removing index 1: ";
      list.display();
 
