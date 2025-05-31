ab84bef82218438bc05acd016e42093b279963e5
majam <majamroczek.pl@gmail.com>
2025-05-31 20:16:53 +0200

    feat: hash table on AVL tree implementation

--- include/collections/hash_tables/hash_table_AVL_tree.cpp
+++ include/collections/hash_tables/hash_table_AVL_tree.cpp
@@ -3,56 +3,62 @@
 
 AVLTree::AVLTree() : root(nullptr), size(0){ }
 
-void AVLTree::clear(const Node* node) {
+void AVLTree::clear(Node* node) {
     if (!node) return;
     clear(node->left);
     clear(node->right);
-    size--;
     delete node;
 }
 
 AVLTree::~AVLTree() {
     clear(root);
+    size =0;
 }
 
-int AVLTree::height(const Node* node)
+int AVLTree::height(Node* node)
 {
     return node ? node->height : 0;
 }
 
-int AVLTree::balance_factor(const Node* node)
+int AVLTree::balance_factor(Node* node)
 {
     return node ? height(node->left) - height(node->right) : 0;
 }
 
-Node* AVLTree::insert(Node* node, int key){
-    size++;
-    if (node == nullptr)
-        return new Node(key);
+Node* AVLTree::insert(Node* node, int key, int value){
+
+    if (node == nullptr) {
+        size++;
+        return new Node(key, value);
+    }
 
     if (key < node->key) {
-        node->left = insert(node->left, key);
+        node->left = insert(node->left, key, value);
     }
     else if (key > node->key) {
-        node -> right = insert(node->right, key);
-    } else
+        node -> right = insert(node->right, key, value);
+    } else {
+        node->value=value;
         return node;
-
+    }
     node->height = std::max(height(node->left), height(node->right)) +1;
-    if (balance_factor(node) > 1 && key < node->left->key) {
-        return right_rotate(node);
+    if (balance_factor(node) > 1) {
+        if (node->left && key < node->left->key)
+            return right_rotate(node);
+        if (node->left && key > node->left->key) {
+            node->left = left_rotate(node->left);
+            return right_rotate(node);
+        }
     }
-    if (balance_factor(node) < -1 && key > node->right->key) {
-        return left_rotate(node);
+    if (balance_factor(node) < -1) {
+        if (node->right && key > node->right->key)
+            return left_rotate(node);
+        if (node->right && key < node->right->key) {
+            node->right = right_rotate(node->right);
+            return left_rotate(node);
+        }
     }
-    if (balance_factor(node) > 1 && key > node->left->key) {
-        node->left = left_rotate(node->left);
-        return right_rotate(node);
-    }
-    if (balance_factor(node) < -1 && key < node->right->key) {
-        node->right = right_rotate(node->right);
-        return left_rotate(node);
-    }
+
     return node;
 }
 
@@ -97,9 +103,11 @@ Node* AVLTree::remove(Node* node, const int key) {
         }
 
         const Node* min = node->right;
-        while (min->left)
+        while (min->left) {
             min = min->left;
+        }
         node->key = min->key;
+        node->value = min->value;
         node->right = remove(node->right, min->key);
     }
 
@@ -120,3 +128,80 @@ Node* AVLTree::remove(Node* node, const int key) {
     }
     return node;
 }
+int AVLTree::getSize() const {
+    return size;
+}
+void AVLTree::displayTree(const Node *node) {
+    if (node != nullptr) {
+        std::cout << "["<<node->key << ": "<<node->value<<"], ";
+        displayTree(node->left);
+        displayTree(node->right);
+    }
+}
+
+void AVLTree::display() const {
+    displayTree(root);
+}
+
+Node* AVLTree::copy( Node* node) {
+    if (!node) return nullptr;
+
+    Node* newNode = new Node(node->key, node->value);
+    newNode->left = copy(node->left);
+    newNode->right = copy(node->right);
+    newNode->height = node->height;
+    return newNode;
+}
+
+void AVLTree::copyFrom(const AVLTree& other) {
+    clear(root);
+    root = copy(other.root);
+    size = other.size;
+}
+
+
+HashTableAVLTree::HashTableAVLTree(int arraySize) {
+    this->arraySize = arraySize;
+    tab = new AVLTree[this->arraySize];
+}
+HashTableAVLTree::~HashTableAVLTree() {
+    delete [] tab;
+}
+int HashTableAVLTree::getHash(int key) const {
+    return key % arraySize;
+}
+
+void HashTableAVLTree::insert(int key, int value) {
+    int index = getHash(key);
+    tab[index].insert(key, value);
+    size++;
+}
+
+void HashTableAVLTree::remove(int key) {
+    int index = getHash(key);
+    tab[index].remove(key);
+    size --;
+}
+
+int HashTableAVLTree::getSize() const {
+    return size;
+}
+
+HashTableAVLTree::HashTableAVLTree(const HashTableAVLTree& other)
+    : size(other.size), arraySize(other.arraySize) {
+
+    tab = new AVLTree[arraySize];
+    for (int i = 0; i < arraySize; ++i) {
+        tab[i].copyFrom(other.tab[i]);
+    }
+}
+
+void HashTableAVLTree::display() const {
+    for (int i = 0; i < arraySize; ++i) {
+        std::cout << "Bucket " << i << ": ";
+        tab[i].display();
+        std::cout << std::endl;
+    }
+}
+
+
--- include/collections/hash_tables/hash_table_AVL_tree.h
+++ include/collections/hash_tables/hash_table_AVL_tree.h
@@ -1,14 +1,17 @@
 #ifndef HASH_TABLE_AVL_TREE_H
 #define HASH_TABLE_AVL_TREE_H
 
+#include <string>
+
 struct Node {
     int key;
-    mutable int height;
-    mutable Node* left;
+    int value;
+    int height;
+    Node* left;
     Node* right;
 
-    explicit Node(int key)
-        : key(key), height(1), left(nullptr), right(nullptr) {}
+    explicit Node(int key, int value)
+        : key(key),value(value), height(1), left(nullptr), right(nullptr) {}
 };
 
 class AVLTree {
@@ -17,24 +20,44 @@ private:
     int size;
 
 
-    static int height(const Node* node);
-    static int balance_factor(const Node* node);
+    static int height(Node* node);
+    static int balance_factor(Node* node);
     static Node* left_rotate(Node* node);
     static Node* right_rotate(Node* node);
-    Node* insert(Node* node, int key);
+    Node* insert(Node* node, int key, int value);
     Node* remove(Node* node, int key);
-
-    void clear(const Node* node);
+    static void displayTree(const Node *node);
+    void clear(Node* node);
 
 
 public:
     AVLTree();
     ~AVLTree();
 
-    void insert(const int key) { root = insert(root, key); }
+    static Node* copy( Node* node);
+    void copyFrom(const AVLTree& other);
+    void insert(const int key, const int value) { root = insert(root, key, value); }
     void remove(const int key) { root = remove(root, key); }
-
+    [[nodiscard]] int getSize() const;
+    void display() const;
 };
 
+class HashTableAVLTree {
+
+private:
+    int size=0;
+    int arraySize;
+    [[nodiscard]] int getHash(int key) const;
+    AVLTree* tab;
+
+public:
+    HashTableAVLTree(const HashTableAVLTree& other);
+    explicit HashTableAVLTree(int arraySize);
+    ~HashTableAVLTree();
+    void insert(int key, int value);
+    void remove(int key);
+    [[nodiscard]] int getSize() const;
+    void display() const;
+};
 
 #endif //HASH_TABLE_AVL_TREE_H
